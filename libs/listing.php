0ce5133a5310ba176db18373e0579b812b23fb2c
Anthony Laplane <anthony.laplane@gmail.com>
2024-12-16 19:32:55 +0100

    filtres live 7

--- libs/listing.php
+++ libs/listing.php
@@ -1,11 +1,44 @@
 <?php
 
-function getListings(PDO $pdo): array
+function getListings(PDO $pdo, array $filters = []): array
 {
+    $orderBy = "listing.id DESC";
+    $relevance = "";
+    $conditions = [];
+    if (isset($filters["search"])) {
+        $match = "MATCH(title) AGAINST(:search)";
+        $conditions[] =  $match;
+        $relevance =  ", $match as relevance";
+        $orderBy = "relevance DESC";
+    }
+    if (isset($filters["min_price"])) {
+        $conditions[] = "price >= :min_price";
+    }
+    if (isset($filters["max_price"])) {
+        $conditions[] = "price <= :max_price";
+    }
+    if (isset($filters["category"])) {
+        $conditions[] = "category_id = :category";
+    }
+    $where = $conditions ? " WHERE " . implode(" AND ", $conditions) : "";
     $sql = "SELECT listing.id, listing.title, listing.description, listing.image, listing.price
-            FROM listing";
+            $relevance
+            FROM listing
+            $where";
 
     $query = $pdo->prepare($sql);
+    if (isset($filters["search"])) {
+        $query->bindValue(":search", "%{$filters["search"]}%");
+    }
+    if (isset($filters["min_price"])) {
+        $query->bindValue(":min_price", $filters["min_price"]);
+    }
+    if (isset($filters["max_price"])) {
+        $query->bindValue(":max_price", $filters["max_price"]);
+    }
+    if (isset($filters["category"])) {
+        $query->bindValue(":category", $filters["category"], PDO::PARAM_INT);
+    }
     $query->execute();
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
